d51b461e61f4042df25d3e9a1923e069d9a438ac
Raul C. Sîmpetru <raul.simpetru@fau.de>
2025-05-27 21:44:20 +0200

    Add core device constants and enums to biosignal device interface

---
+++ biosignal_device_interface/constants/__init__.py
@@ -0,0 +1,12 @@
+"""Constants and enums for biosignal device interfaces."""
+
+# Core device constants
+from biosignal_device_interface.constants.devices.core.base_device_constants import (
+    DeviceType,
+    DEVICE_NAME_DICT,
+)
+
+__all__ = [
+    "DeviceType",
+    "DEVICE_NAME_DICT",
+]
